3e6626ab0c122458490a8cb221094666b972d531
kamimong <kamimong@naver.com>
2019-11-14 23:36:26 +0900

    Add notice

---
+++ app/src/main/java/com/example/se_2019/content_notice.java
@@ -0,0 +1,14 @@
+package com.example.se_2019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class content_notice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_notice);
+    }
+}
