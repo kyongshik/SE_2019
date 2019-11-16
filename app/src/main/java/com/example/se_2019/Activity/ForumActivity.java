426453544d7de760e5c28b6701ee94c433cbefa4
= <smile12_27@naver.com>
2019-11-17 00:06:13 +0900

    상단바 오류 해결

--- app/src/main/java/com/example/se_2019/Activity/ForumActivity.java
+++ app/src/main/java/com/example/se_2019/Activity/ForumActivity.java
@@ -2,11 +2,15 @@ package com.example.se_2019.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.se_2019.Note;
 import com.example.se_2019.R;
@@ -35,7 +39,15 @@ public class ForumActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_forum);
+        setContentView(R.layout.activity_forum);
+        //툴바
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //왼쪽에 home버튼 추가
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.toolbar_home);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         setListView();
 
         //  list_itemArrayList = new ArrayList<Post>();
@@ -95,6 +107,34 @@ public class ForumActivity extends AppCompatActivity {
         Intent intent = new Intent(ForumActivity.this, AddPostActivity.class);
         intent.putExtra("postinfo", postlist);
         startActivityForResult(intent, NEW_POST);
+    }
+    //TOOLBAR설정
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);//여기서 e/menu_main UI를 가져옴
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            Toast.makeText(this, "홈버튼을 눌렀습니다", Toast.LENGTH_SHORT).show();
+
+        }
+        if (id == R.id.toolbar_alarm) {
+            Toast.makeText(this, "알람버튼을 눌렀습니다", Toast.LENGTH_SHORT).show();
+        }
+        if (id == R.id.toolbar_profile) {
+            Toast.makeText(this, "프로필버튼을 눌렀습니다", Toast.LENGTH_SHORT).show();
+            // SharedPreferences preference = PreferenceManager.getDefaultSharedPreferences(this);
+            Intent intent = new Intent(this, Preferences.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
